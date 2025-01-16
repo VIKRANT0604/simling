a504d8dc84fc73e56a21ffa06d4c8eaee31e14ba
VIKRANT0604 <vikrantchoudhary1203@gmail.com>
2025-01-16 18:25:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Support and contact information - Provide your own support channels
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/A_m_11_Thoughts")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Dora_Hub")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/prishus_support")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
